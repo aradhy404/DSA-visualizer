97855f60081ed884103f4d207da467ab2d7f3fb8
aradhy404 <aradhy404@gmail.com>
2026-06-28 01:36:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function generateArray(){
+    const container = document.getElementById('array-container');
+    container.innerHTML = '';
+
+    for(let i = 0;i<20;i++){
+        let height = Math.floor(Math.random()*300) + 30;
+        let bar = document.createElement('div');
+        bar.style.height = height +'px';
+        bar.style.backgroundColor = 'purple';
+        bar.style.width = '30px';
+        bar.style.display = 'block';
+        bar.style.margin = '2px';
+        container.appendChild(bar);
+    }
+}
+async function bubbleSort() {
+    let bars = document.getElementsByTagName('div');
+    bars = Array.from(bars).filter(bar => bar.parentElement.id === 'array-container');
+    
+    for(let i = 0; i < bars.length; i++){
+        for(let j = 0; j < bars.length - i - 1; j++){
+            bars[j].style.backgroundColor = 'red';
+            bars[j+1].style.backgroundColor = 'red';
+            
+            await new Promise(r => setTimeout(r, 100));
+            
+            let h1 = bars[j].style.height;
+            let h2 = bars[j+1].style.height;
+            
+            if(parseInt(h1) > parseInt(h2)){
+                bars[j].style.height = h2;
+                bars[j+1].style.height = h1;
+            }
+            
+            bars[j].style.backgroundColor = 'purple';
+            bars[j+1].style.backgroundColor = 'purple';
+        }
+    }
+}
